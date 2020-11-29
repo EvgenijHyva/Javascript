befa49d229bebebd3396ed0acea18eef3c2adad8
Evgeny <evgeny.hyvarinen@gmail.com>
2020-11-29 23:54:45 +0200

    hw3

---
+++ script_hw5.js
@@ -0,0 +1,15 @@
+console.log("*Задание 5: цикл for без тела.");
+/*
+ * Нарисовать пирамиду с 20 рядами с помощью console.log, как показано на рисунке:
+x
+xx
+xxx
+xxxx
+xxxxx
+*/
+
+let i = "";
+do {
+    i += "x"
+    console.log(i);
+} while (i.length <= 20);
